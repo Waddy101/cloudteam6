4bc3c30930bb0af390c3d8b4c55ab268dd1622cf
Adam Wadsworth <adamwads@btinternet.com>
2017-04-10 15:17:53 +0100

    Fixed uploading of applications to the system, ready for work on advancements now

--- src/main/java/com/cloudteam6/SpringBootWebApplication.java
+++ src/main/java/com/cloudteam6/SpringBootWebApplication.java
@@ -1,11 +1,15 @@
 package com.cloudteam6;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class SpringBootWebApplication extends SpringBootServletInitializer {
 
 	@Override
--- src/main/java/com/cloudteam6/Uploader.java
+++ src/main/java/com/cloudteam6/Uploader.java
@@ -5,19 +5,20 @@ import java.io.File;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
-/**
- * Servlet implementation class Uploader
- */
-@WebServlet("/upload")
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
 @MultipartConfig(fileSizeThreshold=1024*1024*2, //2MB
 					maxFileSize=1024*1024*10,    //10MB
 					maxRequestSize=1024*1024*50)  //50MB
+@Controller
 public class Uploader extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
@@ -35,6 +36,7 @@ public class Uploader extends HttpServlet {
 		return "";
 	}
 	
+	@RequestMapping(value="/upload", method=RequestMethod.POST)
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		String appPath = req.getServletContext().getRealPath("");
 		String savePath = appPath + File.separator + SAVE_DIR;
@@ -47,7 +49,7 @@ public class Uploader extends HttpServlet {
 			Extractor extractor = new Extractor();
 			extractor.extractFile(filePath, fileName);
 		}
-		res.sendRedirect("index.jsp");
+		res.sendRedirect("home.jsp");
 	}
 
 }
