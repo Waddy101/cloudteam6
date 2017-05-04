779a12c4d6a6f978ae92d74504e49cc4f59c9792
Adam Wadsworth <AMWadsworth1@sheffield.ac.uk>
2017-05-04 14:06:18 +0100

    Created user list where an admin can make users devs/admins, needs finishing

--- src/main/java/com/cloudteam6/service/UserService.java
+++ src/main/java/com/cloudteam6/service/UserService.java
@@ -1,5 +1,8 @@
 package com.cloudteam6.service;
 
+
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.cloudteam6.model.User;
@@ -7,6 +10,6 @@ import com.cloudteam6.model.User;
 @Service
 public interface UserService {
     void save(User user);
-
     User findByUsername(String username);
+    List<User> findAll();
 }
--- src/main/java/com/cloudteam6/web/PageController.java
+++ src/main/java/com/cloudteam6/web/PageController.java
@@ -3,7 +3,6 @@ package com.cloudteam6.web;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
